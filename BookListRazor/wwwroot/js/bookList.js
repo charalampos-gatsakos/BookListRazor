5a096f022a7d7a8bf5129bdb67b837562f1fa64c
Daddy-Gats <charalamposgatsakos@gmail.com>
2022-03-20 18:09:45 +0200

    Js Import

---
+++ BookListRazor/wwwroot/js/bookList.js
@@ -0,0 +1,20 @@
+﻿var dataTable;
+
+$(document).ready(Function(){
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_Load').DataTable({
+        "ajax": {
+            "url": "api/book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "colums": [
+            {"data": "name", "with": "30%" },
+            {"data": "name", "with": "30%" },
+            {"data": "name", "with": "30%" },
+        ]
+    })
+}
